3b1019b8e4ff0d28735c45ee0f75315141b23c83
Teodora Neicheva <teodoraivailovaneicheva@gmail.com>
2024-02-07 21:28:51 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Kiril ";
+        String middleName = "Angelov ";
+        String lastName = "Angelov";
+        String fullName = firstName + middleName + lastName;
+        int years = 38;
+        String email = "angelov838@gmail.com";
+        String adress = "G.K Chayka 37a";
+        String phone = "0897408121";
+        String postcode = "9010";
+        String moreInformation = "none";
+
+
+        System.out.println(fullName);
+        System.out.println(years);
+        System.out.println(email);
+        System.out.println(adress);
+        System.out.println(phone);
+        System.out.println(postcode);
+        System.out.println(moreInformation);
+    }
+
+    }
+
